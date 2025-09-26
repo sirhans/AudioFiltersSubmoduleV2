f578e448f8212c9f6d4e6297dc471c7768a9b426
Hans Anderon (SUTD) <sirhans@gmail.com>
2025-09-26 21:02:04 +0700

    Update BMLoopFinder.h

--- AudioFilters/OtherEffects/BMLoopFinder.h
+++ AudioFilters/OtherEffects/BMLoopFinder.h
@@ -28,7 +28,7 @@ typedef struct bmLoopPoints {
  * @param audioBufferLength length of audio buffer in samples
  * @param minLoopLength the minimum acceptable length, in samples, of the loop. Usually this is the wavelength of the sound, minus a few percent in case the pitch isn't stable.
  * @param maxLoopLength must be <= numSamples
- * @param qualityGoal the function will stop searching and return a result immediately if it finds something that is as at least as good as this quality goal. The max value for this is 0.0, which is a perfect loop, like what you get on synth sounds. Any loop that isn't perfect has a negative number for quality. So, for example a really great loop that isn't 100% perfect might be quality of -0.001.
+ * @param qualityGoal the function will stop searching and return a result immediately if it finds something that is as at least as good as this quality goal. The max value for this is 0.0, which is a perfect loop, like what you get on synth sounds. Any loop that isn't perfect has a negative number for quality. So, for example a really great loop that isn't 100% perfect might be quality of -0.001. You can set this to 0.0 if you just want to find the best loop every time. But sometimes that might take too much time. So if you find you can get a loop that's good enough with quality = -0.1, for example, then set it like that and the code will run faster.
  *
  * @returns a pair of loop points, indices in buffer
  */
