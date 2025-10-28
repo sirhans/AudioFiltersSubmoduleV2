fc0c6c04b3df2d1cf59598ff5582f31538f2a1e9
Hans Anderon (SUTD) <sirhans@gmail.com>
2025-10-28 18:21:21 +0700

    BM2x2Matrix switched from simd to arm neon intrinsics

--- AudioFilters/MathUtilities/BM2x2Matrix.c
+++ AudioFilters/MathUtilities/BM2x2Matrix.c
@@ -5,54 +5,142 @@
 #include "BM2x2Matrix.h"
 #include <math.h>
 
+//
+//  BM2x2Matrix.h
+//  BMAudioFilters
+//
+//  Created by Hans on 6/7/16.
+//
+//  Most of this code should be replaced with simd_float 2x2 functions. We have
+//  commented out the functions that should be replaced with simd function
+//  calls. The only functions left are matrix rotation functions, which we have
+//  updated to use the simd_float2x2 datatype.
+//
+//  This file may be used, distributed and modified freely by anyone,
+//  for any purpose, without restrictions.
+//
 
 
-BM2x2Matrix BM2x2Matrix_mul(BM2x2Matrix A, BM2x2Matrix B){
-    // [(B.a1 * A.a1 +  B.b1 * A.a2) (B.a2 * A.a1 +  B.b2 * A.a2)]     [A.a1 A.a2]      [B.a1 B.a2]
-    // [(B.a1 * A.b1 +  B.b1 * A.b2) (B.a2 * A.b1 +  B.b2 * A.b2)]  =  [A.b1 A.b2]   x  [B.b1 B.b2]
-    BM2x2Matrix C = {(B.a1 * A.a1 +  B.b1 * A.a2),(B.a2 * A.a1 +  B.b2 * A.a2),
-                     (B.a1 * A.b1 +  B.b1 * A.b2),(B.a2 * A.b1 +  B.b2 * A.b2)};
-    return C;
-}
-
-BM2x2Matrix BM2x2Matrix_rotationMatrix(float theta){
-//    simd_float2 column0 = {cosf(theta), sinf(theta)};
-//    simd_float2 column1 = {-sinf(theta),cosf(theta)};
-//    simd_float2x2 R = simd_matrix(column0, column1);
-
-    BM2x2Matrix R = {cosf(theta), -sinf(theta),
-                     sinf(theta), cosf(theta)};
-    return R;
-}
-
-
-BM2x2Matrix BM2x2Matrix_rotate(BM2x2Matrix M, float theta){
-    return BM2x2Matrix_mul(M,BM2x2Matrix_rotationMatrix(theta));
-}
-
-
-bool BM2x2Matrix_equal(BM2x2Matrix A, BM2x2Matrix B){
-    if( A.a1 == B.a1 &&
-        A.a2 == B.a2 &&
-        A.b1 == B.b1 &&
-        A.b2 == B.b2 )
-    {
-        return true;
-    }
-
-    return false;
+static inline float32x2x2_t BM2x2Matrix_rotationMatrix(float theta){
+	float32x2_t column0 = {cosf(theta), sinf(theta)};
+	float32x2_t column1 = {-sinf(theta),cosf(theta)};
+	float32x2x2_t R = { column0, column1 };
+	return R;
 }
 
 
 
-/*!
- *
- * @param A 2x2 matrix
- * @param x column vector
- * @return A.x
- */
-BMVector2 BM2x2MVMul(BM2x2Matrix A, BMVector2 X){
-    BMVector2 y = {A.a1 * X.x + A.a2 * X.y,
-                   A.b1 * X.x + A.b2 * X.y};
-    return y;
-}
+
+///*!
+// *BM2x2MatrixD_rotationMatrix
+// *
+// * @abstract returns a 2x2 rotation matrix with angle theta
+// */
+//static inline float64x2x2_t BM2x2MatrixD_rotationMatrix(double theta){
+//	float64x2_t column0 = {cos(theta), sin(theta)};
+//	float64x2_t column1 = {-sin(theta),cos(theta)};
+//	float64x2x2_t R = { column0, column1 };;
+//	return R;
+//}
+
+
+
+
+///*!
+//*BM2x2Matrix_rotate
+//*
+//* @abstract out = M.RotationMatrix(theta)
+//*/
+//static inline float32x2x2_t BM2x2Matrix_rotate(float32x2x2_t M, float theta){
+//	return simd_mul(M,BM2x2Matrix_rotationMatrix(theta));
+//}
+
+
+
+	
+//
+//    /*
+//     * |m11 m12|
+//     * |m21 m22|
+//     */
+//    typedef struct {
+//        float m11,m12,m21,m22;
+//    } BM2x2Matrix;
+//
+//
+//
+//
+//    /*
+//     * |m11 m12|
+//     * |m21 m22|
+//     */
+//    typedef struct {
+//        double m11,m12,m21,m22;
+//    } BM2x2MatrixD;
+//
+//
+//
+//
+//    typedef struct {
+//        float x,y;
+//    } BMVector2;
+//
+//
+//
+//    typedef struct {
+//        double x,y;
+//    } BMVector2D;
+//
+//
+//
+//    // C = A.B
+//    static inline BM2x2Matrix BM2x2Matrix_mmul(BM2x2Matrix A, BM2x2Matrix B){
+//        BM2x2Matrix C = {A.m11*B.m11 + A.m12*B.m21, A.m11*B.m12 + A.m12*B.m22,
+//                         A.m21*B.m11 + A.m22*B.m21, A.m21*B.m12 + A.m22*B.m22 };
+//        return C;
+//    }
+//
+//
+//
+//
+//
+//    // C = A.B
+//    static inline BM2x2MatrixD BM2x2MatrixD_mmul(BM2x2MatrixD A, BM2x2MatrixD B){
+//        BM2x2MatrixD C = {A.m11*B.m11 + A.m12*B.m21, A.m11*B.m12 + A.m12*B.m22,
+//            A.m21*B.m11 + A.m22*B.m21, A.m21*B.m12 + A.m22*B.m22 };
+//        return C;
+//    }
+//
+//
+//
+//
+//    // y = M.x
+//    static inline BMVector2 BM2x2Matrix_mvmul(BM2x2Matrix M, BMVector2 x){
+//        BMVector2 y = {x.x*M.m11 + x.y*M.m12, x.x*M.m21 + x.y*M.m22};
+//        return y;
+//    }
+//
+//
+//
+//
+//
+//    // y = M.x
+//    static inline BMVector2D BM2x2MatrixD_mvmul(BM2x2MatrixD M, BMVector2D x){
+//        BMVector2D y = {x.x*M.m11 + x.y*M.m12, x.x*M.m21 + x.y*M.m22};
+//        return y;
+//    }
+//
+//
+//
+//
+//    // y = sM (scalar matrix multiplication)
+//    static inline BM2x2Matrix BM2x2Matrix_smmul(float s, BM2x2Matrix M){
+//        M.m11 *= s;
+//        M.m12 *= s;
+//        M.m21 *= s;
+//        M.m22 *= s;
+//        return M;
+//    }
+//
+	
+	
--- AudioFilters/MathUtilities/BM2x2Matrix.h
+++ AudioFilters/MathUtilities/BM2x2Matrix.h
@@ -1,24 +1,31 @@
 //
-// Created by hans anderson on 7/4/23.
+//  BM2x2Matrix.h
+//  BMAudioFilters
+//
+//  Created by Hans on 6/7/16.
+//
+//  Most of this code should be replaced with simd_float 2x2 functions. We have
+//  commented out the functions that should be replaced with simd function
+//  calls. The only functions left are matrix rotation functions, which we have
+//  updated to use the simd_float2x2 datatype.
+//
+//  This file may be used, distributed and modified freely by anyone,
+//  for any purpose, without restrictions.
 //
 
-#ifndef THERAPYFILTER_BM2X2MATRIX_H
-#define THERAPYFILTER_BM2X2MATRIX_H
+#ifndef BM2x2Matrix_h
+#define BM2x2Matrix_h
 
-#include <stdbool.h>
+// #define BM2x2Identity {1.0f, 0.0f, 0.0f, 1.0f}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+	
 #include <math.h>
-
-typedef struct matrix2x2 {
-    float a1, a2;
-    float b1, b2;
-} BM2x2Matrix;
-
-
-typedef struct vector2 {
-    float x, y;
-} BMVector2;
-
-
+//#include <simd/simd.h>
+#include <arm_neon.h>
 
 
 /*!
@@ -26,35 +33,118 @@ typedef struct vector2 {
  *
  * @abstract returns a 2x2 rotation matrix with angle theta
  */
-BM2x2Matrix BM2x2Matrix_rotationMatrix(float theta);
+static inline float32x2x2_t BM2x2Matrix_rotationMatrix(float theta);
+
+
+///*!
+// *BM2x2MatrixD_rotationMatrix
+// *
+// * @abstract returns a 2x2 rotation matrix with angle theta
+// */
+//static inline float64x2x2_t BM2x2MatrixD_rotationMatrix(double theta);
+//
+//
+//
+//
+///*!
+//*BM2x2Matrix_rotate
+//*
+//* @abstract out = M.RotationMatrix(theta)
+//*/
+//static inline float32x2x2_t BM2x2Matrix_rotate(float32x2x2_t M, float theta);
 
 
 
 
-/*!
-*BM2x2Matrix_rotate
-*
-* @abstract out = M.RotationMatrix(theta)
-*/
-BM2x2Matrix BM2x2Matrix_rotate(BM2x2Matrix M, float theta);
+	
+//
+//    /*
+//     * |m11 m12|
+//     * |m21 m22|
+//     */
+//    typedef struct {
+//        float m11,m12,m21,m22;
+//    } BM2x2Matrix;
+//
+//
+//
+//
+//    /*
+//     * |m11 m12|
+//     * |m21 m22|
+//     */
+//    typedef struct {
+//        double m11,m12,m21,m22;
+//    } BM2x2MatrixD;
+//
+//
+//
+//
+//    typedef struct {
+//        float x,y;
+//    } BMVector2;
+//
+//
+//
+//    typedef struct {
+//        double x,y;
+//    } BMVector2D;
+//
+//
+//
+//    // C = A.B
+//    static inline BM2x2Matrix BM2x2Matrix_mmul(BM2x2Matrix A, BM2x2Matrix B){
+//        BM2x2Matrix C = {A.m11*B.m11 + A.m12*B.m21, A.m11*B.m12 + A.m12*B.m22,
+//                         A.m21*B.m11 + A.m22*B.m21, A.m21*B.m12 + A.m22*B.m22 };
+//        return C;
+//    }
+//
+//
+//
+//
+//
+//    // C = A.B
+//    static inline BM2x2MatrixD BM2x2MatrixD_mmul(BM2x2MatrixD A, BM2x2MatrixD B){
+//        BM2x2MatrixD C = {A.m11*B.m11 + A.m12*B.m21, A.m11*B.m12 + A.m12*B.m22,
+//            A.m21*B.m11 + A.m22*B.m21, A.m21*B.m12 + A.m22*B.m22 };
+//        return C;
+//    }
+//
+//
+//
+//
+//    // y = M.x
+//    static inline BMVector2 BM2x2Matrix_mvmul(BM2x2Matrix M, BMVector2 x){
+//        BMVector2 y = {x.x*M.m11 + x.y*M.m12, x.x*M.m21 + x.y*M.m22};
+//        return y;
+//    }
+//
+//
+//
+//
+//
+//    // y = M.x
+//    static inline BMVector2D BM2x2MatrixD_mvmul(BM2x2MatrixD M, BMVector2D x){
+//        BMVector2D y = {x.x*M.m11 + x.y*M.m12, x.x*M.m21 + x.y*M.m22};
+//        return y;
+//    }
+//
+//
+//
+//
+//    // y = sM (scalar matrix multiplication)
+//    static inline BM2x2Matrix BM2x2Matrix_smmul(float s, BM2x2Matrix M){
+//        M.m11 *= s;
+//        M.m12 *= s;
+//        M.m21 *= s;
+//        M.m22 *= s;
+//        return M;
+//    }
+//
+	
+	
+#ifdef __cplusplus
+}
+#endif
 
-
-
-/*!
- *
- * @param A
- * @param B
- * @return true if A == B
- */
-bool BM2x2Matrix_equal(BM2x2Matrix A, BM2x2Matrix B);
-
-/*!
- *
- * @param A 2x2 matrix
- * @param x column vector
- * @return A.x
- */
-BMVector2 BM2x2MVMul(BM2x2Matrix A, BMVector2 x);
-
-
-#endif //THERAPYFILTER_BM2X2MATRIX_H
+#endif /* BM2x2Matrix_h */
